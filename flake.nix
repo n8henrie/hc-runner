0baf68bd81448e4e22814ab26faf97957626a406
Nathan Henrie <nate@n8henrie.com>
2024-06-09 20:24:49 -0600

    Format

--- flake.nix
+++ flake.nix
@@ -3,29 +3,33 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    inherit (nixpkgs) lib;
-    systems = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
-    systemClosure = attrs:
-      builtins.foldl' (acc: system:
-        lib.recursiveUpdate acc (attrs system)) {}
-      systems;
-  in
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      systemClosure =
+        attrs: builtins.foldl' (acc: system: lib.recursiveUpdate acc (attrs system)) { } systems;
+    in
     systemClosure (
-      system: let
+      system:
+      let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [self.overlays.default];
+          overlays = [ self.overlays.default ];
         };
         # Placeholder name allows one to enter `nix develop` prior to `Cargo.toml` existing
         name =
-          if builtins.pathExists ./Cargo.toml
-          then ((builtins.fromTOML (builtins.readFile ./Cargo.toml)).package).name
-          else "placeholder";
-      in {
+          if builtins.pathExists ./Cargo.toml then
+            ((builtins.fromTOML (builtins.readFile ./Cargo.toml)).package).name
+          else
+            "placeholder";
+      in
+      {
         overlays = {
           default = self.overlays.${name};
           ${name} = _: prev: {
@@ -38,15 +42,16 @@
           ${name} = pkgs.rustPlatform.buildRustPackage {
             inherit name;
             version =
-              if builtins.pathExists ./Cargo.toml
-              then ((builtins.fromTOML (builtins.readFile ./Cargo.toml)).package).version
-              else "placeholder";
+              if builtins.pathExists ./Cargo.toml then
+                ((builtins.fromTOML (builtins.readFile ./Cargo.toml)).package).version
+              else
+                "placeholder";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
-            nativeBuildInputs = [pkgs.pkg-config];
-            buildInputs = with pkgs; ([openssl]
-              ++ lib.optionals stdenv.isDarwin
-              [darwin.apple_sdk.frameworks.SystemConfiguration]);
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs =
+              with pkgs;
+              ([ openssl ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.SystemConfiguration ]);
             dontUseCargoParallelTests = true;
             doCheck = pkgs.stdenv.isLinux;
           };
